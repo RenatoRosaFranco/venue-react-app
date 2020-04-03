2239763ccbb7702289752a26c0c338420cc65fdf
Renato Rosa Franco <renato_ny@live.com>
2020-04-03 10:38:04 -0300

    Add VenueNFO component section

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import './resources/styles.css';
 
 
-import Header from './components/header_footer/Header';
+import Header   from './components/header_footer/Header';
 import Featured from './components/featured';
+import VenueNfO from './components/venueNfO';
 
 class App extends Component {
   render() {
     return(
     <div className='App' 
       style={{height: '1500px', backgroundColor: 'blue'}}>
-      <Header></Header>
-      <Featured></Featured>
+      <Header/>
+      <Featured/>
+      <VenueNfO/>
     </div>
     )
   }
